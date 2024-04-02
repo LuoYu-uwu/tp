7702023ce3a569790118a2a2b27be068a325ef40
LuoYu-uwu <E0968957@u.nus.edu>
2024-04-02 17:32:58 +0800

    Resolve failed gradle check

--- src/main/java/enumerations/GroceryCommand.java
+++ src/main/java/enumerations/GroceryCommand.java
@@ -1,5 +1,5 @@
 package enumerations;
 
 public enum GroceryCommand {
-    ADD, DEL, EXP, AMT, TH, USE, COST, LIST, LISTC, LOW, HELP, SWITCH, EXIT
+    ADD, DEL, EXP, AMT, TH, USE, COST, LIST, LISTC, LISTE, EXPIRING, LOW, HELP, SWITCH, EXIT
 }

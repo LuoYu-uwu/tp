56ef9653bc786035da1cf7105e13d14507af09ba
LuoYu-uwu <E0968957@u.nus.edu>
2024-04-02 10:47:46 +0800

    Use enums instead of magic literals

--- src/main/java/git/Parser.java
+++ src/main/java/git/Parser.java
@@ -25,6 +25,11 @@ public class Parser {
         isRunning = true;
     }
 
+    enum Command {
+        ADD, EXP, AMT, USE, COST, DEL, LIST, LISTC, HELP, EXIT
+
+    }
+
     /**
      * Processes a command and its details into a valid format for executing relevant code.
      *
@@ -46,50 +51,49 @@ public class Parser {
      */
     public void executeCommand(String[] commandParts) throws GitException {
         assert commandParts.length == 2 : "Command passed in wrong format";
-
-        switch (commandParts[0]) {
-        case "add":
+        Command command = Command.valueOf(commandParts[0].toUpperCase());
+        switch (command) {
+        case ADD:
             String category = ui.promptForCategory();
             int amount = ui.promptForAmount();
             String location = ui.promptForLocation();
-            Grocery grocery = new Grocery(commandParts[1], amount, LocalDate.now(), category, 0, location);
+            double cost = ui.promptForCost();
+            Grocery grocery = new Grocery(commandParts[1], amount, LocalDate.now(), category, cost, location);
             String expiration = ui.promptForExpiration();
-            String cost = ui.promptForCost();
             grocery.setExpiration(expiration);
-            grocery.setCost(cost);
             groceryList.addGrocery(grocery);
             break;
 
-        case "exp":
+        case EXP:
             groceryList.editExpiration(commandParts[1]);
             break;
 
-        case "amt":
-        case "use":
+        case AMT:
+        case USE:
             groceryList.editAmount(commandParts[1], commandParts[0].equals("use"));
             break;
 
-        case "cost":
+        case COST:
             groceryList.editCost(commandParts[1]);
             break;
 
-        case "del":
+        case DEL:
             groceryList.removeGrocery(commandParts[1]);
             break;
 
-        case "list":
+        case LIST:
             groceryList.listGroceries();
             break;
 
-        case "listC":
+        case LISTC:
             groceryList.sortByCost();
             break;
 
-        case "help":
+        case HELP:
             ui.displayHelp();
             break;
 
-        case "exit":
+        case EXIT:
             System.out.println("bye bye!");
             isRunning = false;
             break;

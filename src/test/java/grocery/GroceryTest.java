cf5b13f324155126b8423f70b8210800dc99b266
SharlynLui <sharlynluitm@gmail.com>
2024-04-03 23:18:38 +0800

    Edit Test

--- src/test/java/grocery/GroceryTest.java
+++ src/test/java/grocery/GroceryTest.java
@@ -10,7 +10,7 @@ class GroceryTest {
     @Test
     public void printGrocery_noAmountNoExpiration_leaveEmpty() {
         Grocery grocery = new Grocery("apple", 0, 0, null, "fruit", 0,new Location("Pantry"));
-        String message = "apple (fruit), amount not set, expiration date not set, cost not set, location: Pantry";
+        String message = "apple (fruit), amount not set, expiration date not set, cost not set, location: Pantry, remark not set";
         assertEquals(message, grocery.printGrocery());
     }
 
@@ -18,7 +18,7 @@ class GroceryTest {
     public void printGrocery_costWrongFormat_formattedCost() {
         Grocery grocery = new Grocery("chicken", 1, 0, LocalDate.now().plusDays(1), "meat",1,new Location("Pantry"));
         String message = "chicken (meat)" + ", amount: 1 grams" + ", expiration: "
-                + LocalDate.now().plusDays(1) + ", cost: $1.00, location: Pantry";
+                + LocalDate.now().plusDays(1) + ", cost: $1.00, location: Pantry, remark not set";
         assertEquals(message, grocery.printGrocery());
     }
 
@@ -26,7 +26,7 @@ class GroceryTest {
     public void printGrocery_correctAmtAndExpAndCost() {
         Grocery grocery = new Grocery("chicken", 1, 0, LocalDate.now().plusDays(1), "meat",1.20,new Location("Pantry"));
         String message = "chicken (meat)" + ", amount: 1 grams" + ", expiration: "
-                + LocalDate.now().plusDays(1) + ", cost: $1.20, location: Pantry";
+                + LocalDate.now().plusDays(1) + ", cost: $1.20, location: Pantry, remark not set";
         assertEquals(message, grocery.printGrocery());
     }
 

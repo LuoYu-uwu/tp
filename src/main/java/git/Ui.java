023e173e149adde19aecbeddbf58932376e636fb
LuoYu-uwu <E0968957@u.nus.edu>
2024-04-09 15:39:12 +0800

    Improve checkstyle error

--- src/main/java/git/Ui.java
+++ src/main/java/git/Ui.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import enumerations.ProfileCommand;
 import exceptions.GitException;
 import exceptions.InvalidCommandException;
 import exceptions.InvalidCostException;

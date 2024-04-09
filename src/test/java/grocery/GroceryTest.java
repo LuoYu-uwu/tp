857b6d3c793a0fe94694a98394e97719e4e90026
LuoYu-uwu <E0968957@u.nus.edu>
2024-04-09 14:44:47 +0800

    Fix failed test

--- src/test/java/grocery/GroceryTest.java
+++ src/test/java/grocery/GroceryTest.java
@@ -11,7 +11,7 @@ class GroceryTest {
     public void printGrocery_noAmountNoExpiration_leaveEmpty() {
         Grocery grocery = new Grocery("apple", 0, 0, null, "fruit", 0,new Location("Pantry"));
         String message = "apple (fruit), amount not set, expiration date not set, " +
-                "cost not set, location: Pantry, remark not set";
+                "cost: $0.00, location: Pantry, remark not set";
         assertEquals(message, grocery.printGrocery());
     }
 

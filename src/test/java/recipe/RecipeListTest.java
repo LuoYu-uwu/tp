d147cd2dbe46df721fa94db57d372839852c8cd3
SharlynLui <sharlynluitm@gmail.com>
2024-04-14 20:59:51 +0800

    Gradle Check

--- src/test/java/recipe/RecipeListTest.java
+++ src/test/java/recipe/RecipeListTest.java
@@ -4,7 +4,6 @@ import exceptions.nosuch.NoSuchObjectException;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RecipeListTest {
     @Test

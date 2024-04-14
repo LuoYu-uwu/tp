d8cadce98033d0d6cb00f08be43643ac48f6047a
SharlynLui <sharlynluitm@gmail.com>
2024-04-14 13:30:10 +0800

    Gradle checks

--- src/test/java/recipe/RecipeTest.java
+++ src/test/java/recipe/RecipeTest.java
@@ -1,13 +1,9 @@
 package recipe;
 
-import exceptions.DuplicateException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RecipeTest {
     @Test
